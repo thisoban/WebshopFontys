50a60a5456d47fa0ac162801851795f716c9bc87
thisoban mahalingam <thisoban_1995@hotmail.com>
2019-06-13 19:21:55 +0200

    paar classes toegevoegd

---
+++ DataModel/DataUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel
+{
+   public class DataUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+       public int Role { get; set; }
+    }
+}
